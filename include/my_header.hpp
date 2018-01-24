9affa291d4ff0786d782262d7573df586adee700
eleve <eleve@SI-PC05>
2018-01-24 16:02:18 +0100

    j'ai entamer l'exo 1

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -1,9 +1,18 @@
 //  Copyright (c) 2017 Antoine Tran Tan
 //
+// Copyright (c) 2018 Hugo Fernandes
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
 
+double exo1(double rayon)
+{
+	double aire;
+
+	aire = 3.14*rayon*rayon;
+
+		return aire;
+}
 
 
 #endif // MY_HEADER_HPP
--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,15 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
+// Copyright (c) 2018 Hugo Fernandes
 
 #include <iostream>
 #include <my_header.hpp>
 
 int main()
 {
+	double rl;
+	rl = exo1(10.0);
+
     // Affichage sur un terminal
     std::cout << "Hello World!" << std::endl;
 
